640bd3365ca63b42dc691790d0e8e38435cfe1de
ndewon <duniandewon@gmail.com>
2019-06-27 00:23:43 +0700

    Change UserItem and Navbar to stateless functional components

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,37 +1,32 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-  state = {
-    id: '1',
-    login: 'duniandewon',
-    avatar_url: 'https://avatars1.githubusercontent.com/u/26421125?v=4',
-    html_url: 'https://github.com/duniandewon'
-  };
-
-  render() {
-    const { login, avatar_url, html_url } = this.state;
-    return (
-      <div className='card text-center'>
-        <img
-          src={avatar_url}
-          alt={login}
-          className='round-img'
-          style={{ width: '60px' }}
-        />
-        <h3>{login}</h3>
-        <div>
-          <a
-            href={html_url}
-            className='btn btn-dark btn-sm my-1'
-            rel='noopener noreferrer'
-            target='_blank'
-          >
-            More
-          </a>
-        </div>
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
+  return (
+    <div className='card text-center'>
+      <img
+        src={avatar_url}
+        alt={login}
+        className='round-img'
+        style={{ width: '60px' }}
+      />
+      <h3>{login}</h3>
+      <div>
+        <a
+          href={html_url}
+          className='btn btn-dark btn-sm my-1'
+          rel='noopener noreferrer'
+          target='_blank'
+        >
+          More
+        </a>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
+};
 
 export default UserItem;
