40b7b7e717b9d86e5daaaa201ce3a74edccec17f
ndewon <duniandewon@gmail.com>
2019-06-27 04:51:30 +0700

    Fetch data from github using axios

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      'https://api.github.com/users?client_id=32c0d6990a5ef47eca63&client_secret=9d60ab171db7e6aba6c5d33b5ee783fcd14e6bef'
+    );
+
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,32 +2,10 @@ import React, { Component } from 'react';
 import UserItem from './UserItem';
 
 class Users extends Component {
-  state = {
-    users: [
-      {
-        id: '1',
-        login: 'duniandewon',
-        avatar_url: 'https://avatars1.githubusercontent.com/u/26421125?v=4',
-        html_url: 'https://github.com/duniandewon'
-      },
-      {
-        id: '2',
-        login: 'mojombo',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url: 'https://github.com/mojombo'
-      },
-      {
-        id: '3',
-        login: 'defunkt',
-        avatar_url: 'https://avatars0.githubusercontent.com/u/2?v=4',
-        html_url: 'https://github.com/defunkt'
-      }
-    ]
-  };
   render() {
     return (
       <div style={userStyle}>
-        {this.state.users.map(user => (
+        {this.props.users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
       </div>
