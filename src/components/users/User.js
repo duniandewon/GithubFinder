0c1c778f3dc0859ddb8b4c744e42300f0a010b31
ndewon <duniandewon@gmail.com>
2019-07-18 18:35:00 +0700

    Create reducer and actions and move app level state to context

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,13 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useContext } from 'react';
 import Spiner from '../layout/Spiner';
 import Repos from '../repos/Repos';
 import { Link } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext';
 
-const User = ({ getUser, getUserRepos, loading, user, repos, match }) => {
+const User = ({ match }) => {
+  const githubContext = useContext(GithubContext);
+  const { user, getUser, repos, getUserRepos, loading } = githubContext;
+
   useEffect(() => {
     getUser(match.params.login);
     getUserRepos(match.params.login);
@@ -95,14 +98,6 @@ const User = ({ getUser, getUserRepos, loading, user, repos, match }) => {
       <Repos repos={repos} />
     </Fragment>
   );
-};
-
-User.propTypes = {
-  loading: PropTypes.bool,
-  user: PropTypes.object.isRequired,
-  repos: PropTypes.array.isRequired,
-  getUser: PropTypes.func.isRequired,
-  getUserRepos: PropTypes.func.isRequired
 };
 
 export default User;
--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -1,1 +1,50 @@
 // A place for all actions which will decide what's gonna happen to our state based on an action
+import {
+  SEARCH_USERS,
+  SET_LOADING,
+  CLEAR_USERS,
+  GET_USER,
+  GET_REPOS
+} from '../types';
+
+export default (state, action) => {
+  switch (action.type) {
+    case SEARCH_USERS:
+      return {
+        /** we want to return whatever is already in the state, but state is immutable and we cannot reassign values to it. we have copy the state and add any changes to it by using the spread oprator */
+        ...state,
+        users: action.payload,
+        loading: false
+      };
+
+    case CLEAR_USERS:
+      return {
+        ...state,
+        users: [],
+        loading: false
+      };
+
+    case GET_USER:
+      return {
+        ...state,
+        user: action.payload,
+        loading: false
+      };
+
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: action.payload,
+        loading: false
+      };
+
+    case SET_LOADING:
+      return {
+        ...state,
+        loading: true
+      };
+
+    default:
+      return state;
+  }
+};
