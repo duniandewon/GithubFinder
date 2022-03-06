a8819dab9c4d24520b185b8df26d3b2bdf65cf8a
aboli <duniandewon@gmail.com>
2022-03-06 15:43:26 +0700

    Hit search while typing

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,44 +1,54 @@
-import React, { useState, useContext } from 'react';
-import GithubContext from '../../context/github/githubContext';
-import AlertContext from '../../context/alert/alertContext';
+import React, { useState, useContext, useEffect } from "react";
+import GithubContext from "../../context/github/githubContext";
+import AlertContext from "../../context/alert/alertContext";
 
 const Search = () => {
   const githubContext = useContext(GithubContext);
   const alertContext = useContext(AlertContext);
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
 
-  const onChange = e => setText(e.target.value);
+  useEffect(() => {
+    const timeOut = setTimeout(() => {
+      text && githubContext.searchUsers(text);
+    }, 500);
 
-  const onSubmit = e => {
+    return () => {
+      clearTimeout(timeOut);
+    };
+  }, [text]);
+
+  const onChange = (e) => setText(e.target.value);
+
+  const onSubmit = (e) => {
     e.preventDefault();
 
-    if (text === '') {
-      alertContext.setAlert('Please enter something', 'light');
+    if (text === "") {
+      alertContext.setAlert("Please enter something", "light");
     } else {
       githubContext.searchUsers(text);
-      setText('');
+      setText("");
     }
   };
 
   return (
     <div>
-      <form className='form' onSubmit={onSubmit}>
+      <form className="form" onSubmit={onSubmit}>
         <input
-          type='text'
-          name='text'
-          placeholder='Search users...'
+          type="text"
+          name="text"
+          placeholder="Search users..."
           value={text}
           onChange={onChange}
         />
         <input
-          type='submit'
-          value='Search'
-          className='btn btn-dark btn-block'
+          type="submit"
+          value="Search"
+          className="btn btn-dark btn-block"
         />
       </form>
       {githubContext.users.length > 0 && (
         <button
-          className='btn btn-light btn-block'
+          className="btn btn-light btn-block"
           onClick={githubContext.clearUsers}
         >
           Clear
